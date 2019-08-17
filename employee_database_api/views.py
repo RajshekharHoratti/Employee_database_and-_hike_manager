65097a3aa6d90c034b63a6be10cae17fa33c3912
rajhoratti <rajhoratti@mobifyi.com>
2019-08-17 18:23:15 +0530

    added a company started year condition for adding a employee

--- employee_database_api/views.py
+++ employee_database_api/views.py
@@ -4,6 +4,9 @@ from .models import Employees, Departments, DeptEmp, Titles, Salaries
 from datetime import datetime, timedelta
 
 
+
+COMPANY_STARTED_ON_YEAR = "2015"
+
 TitlesAndSalaries = [
     {
         "title": "STAFF",
@@ -52,31 +55,37 @@ class EmployeeHire(APIView):
                             from_date = datetime.strptime(employee['salary']['from_date'], '%Y-%m-%d').date()
                             today_date = datetime.now().date()
                             employee_age = (today_date - birth_date) // timedelta(days=365.2425)
-                            if employee_age >= 18 and employee_age < 60:
 
-                                add_employee = Employees(emp_no=employee['employee_id'], birth_date=birth_date,
-                                                         first_name=employee['first_name'],
-                                                         last_name=employee['last_name'], gender=employee['gender'],
-                                                         hire_date=hire_date)
-                                add_employee.save()
-                                check_department = Departments.objects.get(dept_no=employee['department'])
-                                add_dept_emp = DeptEmp(emp_no=add_employee, dept_no=check_department, from_date=from_date,
+                            hire_year = hire_date.year
+
+                            if str(hire_year) == COMPANY_STARTED_ON_YEAR:
+                                if employee_age >= 18 and employee_age < 60:
+
+                                    add_employee = Employees(emp_no=employee['employee_id'], birth_date=birth_date,
+                                                             first_name=employee['first_name'],
+                                                             last_name=employee['last_name'], gender=employee['gender'],
+                                                             hire_date=hire_date)
+                                    add_employee.save()
+                                    check_department = Departments.objects.get(dept_no=employee['department'])
+                                    add_dept_emp = DeptEmp(emp_no=add_employee, dept_no=check_department, from_date=from_date,
+                                                           to_date=today_date)
+                                    add_dept_emp.save()
+
+                                    add_salaries = Salaries(emp_no=add_employee, salary=employee['salary']['salary_to_date'],
+                                                            from_date=from_date,
+                                                            to_date=today_date)
+                                    add_salaries.save()
+
+                                    add_title = Titles(emp_no=add_employee,
+                                                       title=employee['title'],
+                                                       from_date=from_date,
                                                        to_date=today_date)
-                                add_dept_emp.save()
-
-                                add_salaries = Salaries(emp_no=add_employee, salary=employee['salary']['salary_to_date'],
-                                                        from_date=from_date,
-                                                        to_date=today_date)
-                                add_salaries.save()
-
-                                add_title = Titles(emp_no=add_employee,
-                                                   title=employee['title'],
-                                                   from_date=from_date,
-                                                   to_date=today_date)
-                                add_title.save()
-                                success_employees.append(employee['employee_id'])
+                                    add_title.save()
+                                    success_employees.append(employee['employee_id'])
+                                else:
+                                    print("Invalid Age")
                             else:
-                                print("Invalid Age")
+                                print("Invalid Hire Year")
                         else:
                             print("Invalid Salary")
                     else:
